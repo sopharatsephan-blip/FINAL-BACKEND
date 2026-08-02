300388270e7145c23a3d6d90c9419da97a881abc
sopharatsephan-blip <sopharatsephan@gmail.com>
2026-08-02 20:49:59 +0700

    55555

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '1111',
+  password: '1234',
   database: 'video_summary_g15',
-  port: 3306,
+  port: 3307,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
